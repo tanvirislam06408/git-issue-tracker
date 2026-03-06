0c07a690e2ee69c0a91153ed1af717e75d95935d
tanvirislam <mstanvirislam05@gmail.com>
2026-03-06 23:57:20 +0600

    made a normal card and load dynamic data

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-console.log('hi');
+
+const loadAllIssuesCard=async()=>{
+const res=await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+const data=await res.json();
+console.log(data);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+loadAllIssuesCard();
